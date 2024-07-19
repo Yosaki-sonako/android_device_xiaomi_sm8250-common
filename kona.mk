f61fd6ed0bfb7048774fb7dcc99716e7dffb5152
Rocky7842 <eric.rocky7842@gmail.com>
2024-07-19 22:06:46 +0800

    sm8250-common: Switch Lineage Health HAL to bypass
    Change-Id: I6c899c0822dbbda253a5e091625fd3831410cb94

--- kona.mk
+++ kona.mk
@@ -248,7 +248,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+
 
 # Media configs
 PRODUCT_PACKAGES += \
