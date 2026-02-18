bc19437a877721d4f67e59254cdb0880a1e71b3e
Yosaki´Sonako <79687386+Yosaki-sonako@users.noreply.github.com>
2026-02-19 00:36:09 +0800

    sm8250-common: onOptionsItemSelected method
    Removed onOptionsItemSelected method to simplify navigation handling.

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
@@ -38,15 +38,4 @@ public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
                 .commit();
 
     }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return false;
-
-    }
 }
